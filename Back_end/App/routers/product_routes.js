d510be4a9bb623d7c740737252b82a9c0986d4f1
Umayanga <umayangasai.20@uom.lk>
2022-12-23 17:31:36 +0530

    new update

---
+++ Back_end/App/routers/product_routes.js
@@ -0,0 +1,12 @@
+module.exports=app=>{
+
+// 
+const productController=require("../controllers/product_controller.js");
+
+//localhost:8081/api/product/get:hudfhuh
+var product_routes=require("express").Router();
+
+//Get cart item 
+
+
+}
