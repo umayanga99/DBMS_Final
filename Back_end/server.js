1adb8cd4b15b897d674ae1c0d76906446006c425
Shehan <harshanashehan52@gmail.com>
2022-12-23 22:54:45 +0530

    d

--- Back_end/server.js
+++ Back_end/server.js
@@ -25,6 +25,7 @@ require('./app/routes/cart_routers.js')(app);
 require('./app/routes/product_routers.js')(app);
 require('./app/routers/login_routers.js')(app);
 require('./app/routers/register_routers.js')(app);
+require('./app/routers/manager_routers.js')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8081;
