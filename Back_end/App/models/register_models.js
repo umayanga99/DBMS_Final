9d4219e0478b6eb506f41aed2ec8ea58ea4c5c9b
Shehan <harshanashehan52@gmail.com>
2022-12-23 21:43:28 +0530

    f

---
+++ Back_end/App/models/register_models.js
@@ -0,0 +1,25 @@
+const mysql = require("./db.js");
+
+// constructor
+const newUser = function(file) {
+    this.name = file.name;
+    this.password = file.password;
+    this.email = file.email;
+    this.type = file.type;
+    this.role = file.role;
+    this.id = file.id;
+};
+
+newUser.addUser = (name, password, email, type, role, id) => {
+    mysql.query(`Sign_authentication (${email}, ${password}, ${name}, ${type}, ${id}, ${role})`, (err,res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        } else {
+            result(null, res);
+        }
+    });
+};
+
+module.exports = newUser;
