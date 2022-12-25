53bbd4f16a9caeabac02e99ec1f0ed1dce2bbb16
Shehan <harshanashehan52@gmail.com>
2022-12-25 08:33:50 +0530

    dfsdd

--- Back_end/App/models/order_models.js
+++ Back_end/App/models/order_models.js
@@ -1,12 +1,37 @@
  const mysql = require("./db.js");
 
  const Order=function(file){
-     this.title = file.title;
-     this.prefered_dilivery_date = file.prefered_dilivery_date;
-    
+    this.title = file.title;
+    this.prefered_dilivery_date = file.prefered_dilivery_date;
+    this.product_ID = file.product_ID;
+    this.cart_ID = file.cart_ID;
+    this.email = file.email;
+    this.order_ID = file.order_ID;
+    this.address = file.address;
+    this.customer_email = file.customer_email;
+    this.quantity = file.quantity;
+    this.date = file.date;
+    this.time = file.time;
+    this.route_ID = file.route_ID;
+    this.nearest_Branch = file.nearest_Branch;
+    this.tot_capacity = file.tot_capacity;
+    this.state = file.state;
+    this.destination = file.destination;
  };
 
- Order.validateDate = (prefered_dilivery_date) => {
+ Order.validateDate = (product_ID,cart_ID,email,order_ID,address,customer_email,quantity,date,time,route_ID,nearest_Branch,tot_capacity,state,destination) => {
+    mysql.query(`SELECT payment_proceed(${product_ID}, ${cart_ID}, ${email}, ${order_ID}, ${address}, ${customer_email}, ${quantity}, ${date}, ${time}, ${route_ID}, ${nearest_Branch}, ${tot_capacity}, ${state}, ${destination})`, (err,res) => {
+        if(err){
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        } else {
+            result(null, res);
+        }
+    });
+};
+
+Order.validate_Date = (prefered_dilivery_date) => {
     mysql.query(`SELECT validate_Day(${prefered_dilivery_date})`, (err,res) => {
         if(err){
             console.log("error: ", err);
