e04d70a18b2d9f2c1ab193a5a569c2d58c2d4b48
Umayanga <umayangasai.20@uom.lk>
2022-12-29 19:32:51 +0530

    working update

--- Back_end/server.js
+++ Back_end/server.js
@@ -23,14 +23,14 @@ app.get("/", (req, res) => {
 
 require('./app/routers/cart_routers.js')(app);
 require("./app/routers/product_routers.js")(app);
-require("./app/routers/login_routers.js")(app);
+require("./app/routers/auth_routers.js")(app);
 require("./app/routers/manager_routers.js")(app);
 require("./app/routers/orders_routers.js")(app);
-require("./app/routers/login_routers.js")(app);
-require("./app/routers/register_routers.js")(app);
+// require("./app/routers/login_routers.js")(app);
+// require("./app/routers/register_routers.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ||8081 ;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
