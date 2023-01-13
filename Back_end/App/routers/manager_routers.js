ec03a3624551a9ad7d49ddcf0294d55709b15b69
Umayanga <umayangasai.20@uom.lk>
2023-01-13 15:56:44 +0530

    aaa

--- Back_end/App/routers/manager_routers.js
+++ Back_end/App/routers/manager_routers.js
@@ -7,7 +7,7 @@ module.exports = app => {
     managerRouter.get("/getTruckSchedule",ManagerController.getTruckSchedule);
     managerRouter.get("/getAssistantSchedule",ManagerController.getAssistantSchedule);
     managerRouter.get("/getDriverSchedule",ManagerController.getDriverSchedule);
-    managerRouter.post("/getgetMostOrderReport",ManagerController.getMostOrderReport);
+    managerRouter.post("/getMostOrderReport",ManagerController.getMostOrderReport);
     managerRouter.post("/getQuarterlySalesReport",ManagerController.getQuarterlySalesReport);
     managerRouter.post("/getQuarterlyOrderReport",ManagerController.getQuarterlyOrderReport);
     managerRouter.post("/getCustomerOrderReport",ManagerController.getCustomerOrderReport);
